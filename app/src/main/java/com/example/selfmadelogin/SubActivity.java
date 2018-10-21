467701d341c667be6845664ffc44d1f3ff804445
aki0207 <forever.look.aki@docome.ne.jp>
2018-10-21 14:06:32 +0900

    バリデーションを追加

--- app/src/main/java/com/example/selfmadelogin/SubActivity.java
+++ app/src/main/java/com/example/selfmadelogin/SubActivity.java
@@ -5,14 +5,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
-
-import java.sql.SQLData;
 
 public class SubActivity extends AppCompatActivity {
 
